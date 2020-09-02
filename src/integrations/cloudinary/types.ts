34a018e2f46b7a92ec2cc63c98f8765c3f5014cb
DonatasD <donatasdaubar@gmail.com>
2020-09-02 15:04:08 +0300

    Remove TODO

--- src/integrations/cloudinary/types.ts
+++ src/integrations/cloudinary/types.ts
@@ -1,7 +1,6 @@
 import {ResourceApiResponse} from 'cloudinary';
 
 export interface CloudinaryResponse extends ResourceApiResponse {
-  // TODO could max additional fields
   next_cursor: string;
   rate_limit_remaining: number;
   rate_limit_reset_at: Date;
